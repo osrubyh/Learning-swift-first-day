e6012926ab23539258cca5e33f7e1b793f67cee9
Your Name <you@example.com>
2015-12-25 09:29:41 +0800

    第一次commit

---
+++ HiSwift.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+
+
+var hscn=42
+hscn=50
+let myConstant=55
+
+let exDouble: Double = 70
+let exFloat: integer_t = 50
+
+
+let label="Hello World!"
+let width = 94
+let widthLable=label + String(width)
+let widLable = label + String(width)
+let widll = width + integer_t(exDouble)
+
+//把值转换成字符串的方法
+let apples = 3
+let oranges = 5
+let appleSummary = "i have \(oranges) apples"
+let fruitSummary = "i have \(apples + oranges) pieces of fruit."
+
+//使用\()来把一个浮点计算转成
+let ostea = "ChengChao"
+let string = "hello"
+let sayHelloToOstea = string + ostea
+let sayHelloToOsteaForCount = "i sayHelloToOsteaForCount is \(apples + oranges)"
+
+//使用方括号[]来创建数组和字典，并使用下标或者键（key）来访问元素。最后一个元素后面允许有个逗号
+var shoppingList = ["catfish","water","tulips","blue paint"]
+shoppingList[1] = "bottle of water"
+print(shoppingList) //此处值已经改变
+var occupations = [
+    "Malcom" : "Captain",
+    "Katlee" : "Mechanic",
+]
